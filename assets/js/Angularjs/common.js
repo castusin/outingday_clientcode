8552598f669e164a6416ba015f346b4c9b5cf4ac
digitalhelathcare <arcturuscare@gmail.com>
2017-04-11 20:47:49 -0700

    updated od web code

--- assets/js/Angularjs/common.js
+++ assets/js/Angularjs/common.js
@@ -35,14 +35,14 @@ app.controller('MainCtr',['$scope','$state','$window','$rootScope',function ($sc
         debugger;
 
 
-       var area=  $scope.details.address_components[0].types[1];
-        var city=  $scope.details.address_components[1].types[0];
-        var dist=   $scope.details.address_components[2].types[0];
-        var state=  $scope.details.address_components[3].types[0] ;
-        var country=  $scope.details.address_components[4].types[0];
+       // var area=  $scope.details.address_components[0].types[1];
+        // var city=  $scope.details.address_components[1].types[0];
+        // var dist=   $scope.details.address_components[2].types[0];
+        // var state=  $scope.details.address_components[3].types[0] ;
+        // var country=  $scope.details.address_components[4].types[0];
 
-        var lat=  $scope.details.geometry.location.lat() ;
-        var lng=  $scope.details.geometry.location.lng() ;
+        // var lat=  $scope.details.geometry.location.lat() ;
+        // var lng=  $scope.details.geometry.location.lng() ;
 
 
 
