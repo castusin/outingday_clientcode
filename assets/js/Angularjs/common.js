98f3800a0f47c70fa4cd17dc20e7bbd15ef966fc
digitalhelathcare <arcturuscare@gmail.com>
2017-04-11 17:48:24 -0700

    values

--- assets/js/Angularjs/common.js
+++ assets/js/Angularjs/common.js
@@ -35,6 +35,17 @@ app.controller('MainCtr',['$scope','$state','$window','$rootScope',function ($sc
         debugger;
 
 
+       var area=  $scope.details.address_components[0].types[1];
+        var city=  $scope.details.address_components[1].types[0];
+        var dist=   $scope.details.address_components[2].types[0];
+        var state=  $scope.details.address_components[3].types[0] ;
+        var country=  $scope.details.address_components[4].types[0];
+
+        var lat=  $scope.details.geometry.location.lat() ;
+        var lng=  $scope.details.geometry.location.lng() ;
+
+
+
         $rootScope.searchInput = $scope.result;
 
 
